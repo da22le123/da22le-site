cd26f3c9f55acc1dfbecc3cb59fedeeb1f347d6e
da22le123 <ilia1892006@gmail.com>
2025-05-30 20:57:46 +0200

    changes front page

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -1,13 +1,22 @@
 import express from 'express';
+import cors from 'cors';
 import { errorHandler } from './middleware/errorHandler';
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:5173',   // or '*' to allow all
+  methods: ['GET','POST','PUT','DELETE','OPTIONS'],
+  allowedHeaders: ['Content-Type','Authorization']
+}));
+
+
+app.use(express.json()); 
 
 app.get('/', (req, res) => {
   res.send('hello world')
 })
 
-app.use(express.json());
 
 // Global error handler (should be after routes)
 app.use(errorHandler);
