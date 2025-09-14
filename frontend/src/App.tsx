5ff963470b4693e66015a9156594fd8bc3232872
da22le123 <ilia1892006@gmail.com>
2025-09-14 19:09:30 +0200

    fisrt probe of resume page

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './pages/landingPage/LandingPage';
 import PortfolioPage from './pages/portfolioPage/PortfolioPage'
+import ResumePage from './pages/resumePage/ResumePage';
 import "./App.css"
 
 const App: React.FC = () => (
@@ -9,6 +10,7 @@ const App: React.FC = () => (
     <Routes>
       <Route path='/' element={<LandingPage />} />
       <Route path='/portfolio' element={<PortfolioPage />} />
+      <Route path='/resume' element={<ResumePage />} />
       {/* 
         Add more routes here if needed, e.g.:
         <Route path='/about' element={<AboutPage />} />
